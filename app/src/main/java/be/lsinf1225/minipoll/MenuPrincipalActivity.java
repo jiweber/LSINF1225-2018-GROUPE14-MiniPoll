96d29184e8824ea833293e31d994f2f4554cf7ce
BellonGuillaume <33180570+BellonGuillaume@users.noreply.github.com>
2018-04-25 15:40:31 +0200

    Changement des polices des boutons et de la couleur de fond du menu 25-04-2017 à 15:40

--- app/src/main/java/be/lsinf1225/minipoll/MenuPrincipalActivity.java
+++ app/src/main/java/be/lsinf1225/minipoll/MenuPrincipalActivity.java
@@ -1,10 +1,13 @@
 package be.lsinf1225.minipoll;
 
 //import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.QuickContactBadge;
+import android.widget.TextView;
 //import android.widget.TextView;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
@@ -17,7 +20,7 @@ public class MenuPrincipalActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu_principal);
         btn_new = findViewById(R.id.button4);
         btn_quest = findViewById(R.id.button2);
         btn_sond = findViewById(R.id.button5);
@@ -25,5 +28,30 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         amis = findViewById(R.id.quickContactBadge2);
         profil = findViewById(R.id.quickContactBadge);
 
+        setFontBut(btn_aide, "GeosansLight.ttf");
+        setFontBut(btn_new, "GeosansLight.ttf");
+        setFontBut(btn_sond, "GeosansLight.ttf");
+        setFontBut(btn_quest, "GeosansLight.ttf");
+
+    }
+    public void setFontTxt(TextView textView, String fontName) {
+        if(fontName != null){
+            try {
+                Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/" + fontName);
+                textView.setTypeface(typeface);
+            } catch (Exception e) {
+                Log.e("FONT", fontName + " not found", e);
+            }
+        }
+    }
+    public void setFontBut(Button button, String fontName) {
+        if(fontName != null){
+            try {
+                Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/" + fontName);
+                button.setTypeface(typeface);
+            } catch (Exception e) {
+                Log.e("FONT", fontName + " not found", e);
+            }
+        }
     }
 }
