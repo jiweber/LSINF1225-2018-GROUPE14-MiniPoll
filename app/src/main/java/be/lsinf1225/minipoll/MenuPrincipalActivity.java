b3f019073e67af37437a17739b25718088381641
BellonGuillaume <33180570+BellonGuillaume@users.noreply.github.com>
2018-04-25 17:46:37 +0200

    Changement des polices des boutons et de la couleur de fond du menu 25-04-2017 à 15:40

--- app/src/main/java/be/lsinf1225/minipoll/MenuPrincipalActivity.java
+++ app/src/main/java/be/lsinf1225/minipoll/MenuPrincipalActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.QuickContactBadge;
 import android.widget.TextView;
@@ -15,8 +16,8 @@ public class MenuPrincipalActivity extends AppCompatActivity {
     Button btn_sond;
     Button btn_quest;
     Button btn_aide;
-    QuickContactBadge amis;
-    QuickContactBadge profil;
+    Button btn_friends;
+    Button btn_profil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,15 +26,62 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         btn_quest = findViewById(R.id.button2);
         btn_sond = findViewById(R.id.button5);
         btn_aide = findViewById(R.id.button3);
-        amis = findViewById(R.id.quickContactBadge2);
-        profil = findViewById(R.id.quickContactBadge);
+        btn_friends = findViewById(R.id.button);
+        btn_profil = findViewById(R.id.button6);
 
         setFontBut(btn_aide, "GeosansLight.ttf");
         setFontBut(btn_new, "GeosansLight.ttf");
         setFontBut(btn_sond, "GeosansLight.ttf");
         setFontBut(btn_quest, "GeosansLight.ttf");
 
+        btn_profil.setOnClickListener(btn_profil_ls);
+        btn_friends.setOnClickListener(btn_friends_ls);
+        btn_aide.setOnClickListener(btn_aide_ls);
+        btn_sond.setOnClickListener(btn_sond_ls);
+        btn_quest.setOnClickListener(btn_quest_ls);
+        btn_new.setOnClickListener(btn_new_ls);
+
+
+
     }
+
+    View.OnClickListener btn_profil_ls = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.i("DEBUG","Bouton profil");
+        }
+    };
+    View.OnClickListener btn_new_ls = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.i("DEBUG","Bouton nouveau");
+        }
+    };
+    View.OnClickListener btn_quest_ls = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.i("DEBUG","Bouton questionnaire");
+        }
+    };
+    View.OnClickListener btn_sond_ls = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.i("DEBUG","Bouton sondage");
+        }
+    };
+    View.OnClickListener btn_friends_ls = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.i("DEBUG","Bouton amis");
+        }
+    };
+
+    View.OnClickListener btn_aide_ls = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.i("DEBUG","Bouton aide");
+        }
+    };
     public void setFontTxt(TextView textView, String fontName) {
         if(fontName != null){
             try {
