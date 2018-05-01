ee8d09a19a1ee3511a35812929607f7fb9975ac7
BellonGuillaume <33180570+BellonGuillaume@users.noreply.github.com>
2018-05-01 11:46:21 +0200

    Layout de l'activité de connexion et Lien simple entre l'activité de connexion et le menu principal
    01/05/18 - 11:46

--- app/src/main/java/be/lsinf1225/minipoll/ConnexionActivity.java
+++ app/src/main/java/be/lsinf1225/minipoll/ConnexionActivity.java
@@ -1,13 +1,16 @@
 package be.lsinf1225.minipoll;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class ConnexionActivity extends AppCompatActivity {
 
+    ImageView poll;
     EditText et_password;
     EditText et_mail;
     Button btn_connexion;
@@ -17,6 +20,7 @@ public class ConnexionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+        poll = findViewById(R.id.poll);
         et_password = (EditText) findViewById(R.id.et_password);
         et_mail= (EditText) findViewById(R.id.et_mail);
         btn_connexion = (Button) findViewById(R.id.btn_connexion);
@@ -24,12 +28,7 @@ public class ConnexionActivity extends AppCompatActivity {
 
 
 
-        btn_connexion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO
-            }
-        });
+        btn_connexion.setOnClickListener(co);
 
         btn_inscription.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +37,14 @@ public class ConnexionActivity extends AppCompatActivity {
             }
         });
     }
+
+    View.OnClickListener co = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent menu = new Intent(getApplicationContext(),MenuPrincipalActivity.class);
+            startActivity(menu);
+            finish();
+        }
+    };
 
 }
