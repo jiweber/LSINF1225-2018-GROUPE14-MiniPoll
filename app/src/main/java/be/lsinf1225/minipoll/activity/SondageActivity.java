e4f706ea0d8caf448266d37a44473fba6a71c217
LouisDeVogeleer <louis.devogeleer@student.uclouvain.be>
2018-05-05 13:08:32 +0200

    MySondageActivity renomméé

--- app/src/main/java/be/lsinf1225/minipoll/activity/SondageActivity.java
+++ app/src/main/java/be/lsinf1225/minipoll/activity/SondageActivity.java
@@ -2,6 +2,8 @@ package be.lsinf1225.minipoll.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import be.lsinf1225.minipoll.R;
 import be.lsinf1225.minipoll.model.Sondage;
@@ -9,10 +11,22 @@ import be.lsinf1225.minipoll.model.Sondage;
 public class SondageActivity extends AppCompatActivity {
 
     private Sondage sondage;
+    private ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sondage);
         sondage = (Sondage) getIntent().getSerializableExtra("serializabled_sondage");
+
+        TextView textView = findViewById(R.id.lv_sondage);
+        String[] remFriends = sondage.getRemainingFriends(); //TODO
+        String message = "Personnes n'ayant pas encore répondu : \n";
+        message += remFriends[0];
+        for(int i=1; i<remFriends.length; i++){
+            message += ", " + remFriends[i];
+        }
+        textView.setText(message);
+
+        listview = (ListView) findViewById(R.id.lv_sondage);
     }
 }
