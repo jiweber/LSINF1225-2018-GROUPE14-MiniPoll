f750e88309ae7d5b7b9388aa8aafd485e48f1cff
maxiswiz <36965787+maxiswiz@users.noreply.github.com>
2018-05-05 19:28:27 +0200

    ListView pour les questionnaires
    compilation ok mais l'app crash quand l'activité questionnaire est lancée :(

--- app/src/main/java/be/lsinf1225/minipoll/activity/MyQuestnrActivity.java
+++ app/src/main/java/be/lsinf1225/minipoll/activity/MyQuestnrActivity.java
@@ -7,20 +7,35 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import be.lsinf1225.minipoll.R;
+import java.util.List;
 
+import be.lsinf1225.minipoll.R;
+import be.lsinf1225.minipoll.model.Questnr;
 
 
 public class MyQuestnrActivity extends AppCompatActivity {
+
+    private ListView qstnr;
+
+    List<String> u_qstnr= Questnr.getTtitles();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_myquestnr);
+
+        qstnr=findViewById(R.id.listQstrn);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1);
+
+        qstnr.setAdapter(adapter);
 
     }
 }
