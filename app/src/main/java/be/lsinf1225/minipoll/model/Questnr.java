6a30346f451dc5b6a0829ee857baeee545f66970
maxiswiz <36965787+maxiswiz@users.noreply.github.com>
2018-05-08 15:15:09 +0200

    récupération du nbr de questions dans un questionnaire

--- app/src/main/java/be/lsinf1225/minipoll/model/Questnr.java
+++ app/src/main/java/be/lsinf1225/minipoll/model/Questnr.java
@@ -3,8 +3,6 @@ package be.lsinf1225.minipoll.model;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
-import android.util.SparseArray;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,17 +16,21 @@ public class Questnr {
     private String TitreQstnr;
     private String AuteurQstnr;
     private int ScoreQstnr;
+    private int nbrQuestions;
 
-    private static SparseArray<Questnr> questnrSparseArray = new SparseArray<>();
+    //private static SparseArray<Questnr> questnrSparseArray = new SparseArray<>();
 
-    private Questnr(int IDquestnr, String titre, String auteur,int score) {
+    private Questnr(int IDquestnr, String titre, String auteur,int score,int nbr_qst) {
         this.IDQstnr = IDquestnr;
         this.TitreQstnr = titre;
         this.AuteurQstnr = auteur;
         this.ScoreQstnr = score;
-        Questnr.questnrSparseArray.put(IDquestnr,this);
+        this.nbrQuestions = nbr_qst;
+        //Questnr.questnrSparseArray.put(IDquestnr,this);
     }
 
+
+    //getters
     public int getIDQstnr() {
         return IDQstnr;
     }
@@ -43,6 +45,15 @@ public class Questnr {
 
     public int getScoreQstnr() { return ScoreQstnr; }
 
+    public int getNbrQuestions() {
+        return nbrQuestions;
+    }
+
+
+    // incrémentation de Statut: à utiliser lorsque le ClinOnItem se produit
+    // to do
+
+    // insntanciations des questionnaires pour le user courant dans un Arraylist
     public static ArrayList<Questnr> getSQLQuestnr(){
 
         ArrayList<Questnr> questnrs = new ArrayList<Questnr>();
@@ -51,10 +62,17 @@ public class Questnr {
 
         String mail = MiniPoll.getConnected_user().getMail();
 
-        String sql = "SELECT Q.IDquestionnaire, Q.Titre, Q.Auteur, PQ.Score" +
-                " FROM Questionnaire Q, Participation_questionnaire PQ" +
-                " WHERE Q.IDquestionnaire=PQ.IDquestionnaire and PQ.mail='"+mail+"'"+";";
+        String sqlMain = "SELECT Q.IDquestionnaire, Q.Titre, Q.Auteur, PQ.Score, nbr_qst" +
+                " FROM Questionnaire Q, Participation_questionnaire PQ, " +
+                "(SELECT Qstnr.IDquestionnaire, count(*) as nbr_qst" +
+                " FROM Questionnaire qstnr,Question qst" +
+                " WHERE qstnr.IDquestionnaire=qst.IDquestionnaire" +
+                " GROUP BY Qstnr.IDQuestionnaire) as cnt" +
+                " WHERE Q.IDquestionnaire=PQ.IDquestionnaire and cnt.IDquestionnaire=Q.IDquestionnaire" +
+                " and PQ.mail='" +mail+"';";
 
+        // querry utiliser pour vérifier que la table n'est pas vide
+        // et que donc le user courant participe actuelement à des questionnaires
         String sqlCount = "SELECT count(*) FROM (SELECT Q.IDquestionnaire, Q.Titre, Q.Auteur, PQ.Score" +
                 " FROM Questionnaire Q, Participation_questionnaire PQ" +
                 " WHERE Q.IDquestionnaire=PQ.IDquestionnaire and PQ.mail='"+mail+"');";
@@ -62,26 +80,33 @@ public class Questnr {
         count.moveToFirst();
 
         if(count.getInt(0)==0){
-            questnrs.add(new Questnr(0,"Pas de Questionnaire en cours",null,0));
+            questnrs.add(new Questnr(0,"Pas de Questionnaire en cours",null,0,0));
         } else {
-            Cursor c = db.rawQuery(sql,null);
+            // récupération de l'IDquestionnaire, le titre, l'auteur du score et du nbr de questions
+            Cursor c = db.rawQuery(sqlMain,null);
             c.moveToFirst();
             while (!c.isAfterLast()){
                 int id = c.getInt(0);
                 String titre = c.getString(1);
                 String auteur = c.getString(2);
                 int score = c.getInt(3);
+                int nbr = c.getInt(4);
 
-                Questnr questnr = Questnr.questnrSparseArray.get(id);
+                /*Questnr questnr = Questnr.questnrSparseArray.get(id);
                 if(questnr==null){
                     questnr = new Questnr(id,titre,auteur,score);
-                }
+                }*/
+
+                Questnr questnr = new Questnr(id,titre,auteur,score,nbr);
 
                 questnrs.add(questnr);
                 c.moveToNext();
+
             }
 
             c.close();
+
+
         }
 
         count.close();
@@ -90,6 +115,7 @@ public class Questnr {
         return questnrs;
     }
 
+    // methode pour simplifier l'affichage des sujest dans l'activité MyQuestnrActivity
     public static List<String> getTtitles(){
         List<String> titles = new ArrayList<String>();
         ArrayList<Questnr> qst = Questnr.getSQLQuestnr();
@@ -103,7 +129,8 @@ public class Questnr {
         String resume = "ID: " + Integer.toString(IDQstnr)
                         + " Titre: " + TitreQstnr
                         + " Auteur: " + AuteurQstnr
-                        + " Score: " + ScoreQstnr;
+                        + " Score: " + ScoreQstnr
+                        + " Nbr de questions: " + nbrQuestions;
         return resume;
     }
 
