84d5e6adf04dfbd14c4fe15d51a5b52fa2bf8928
BellonGuillaume <33180570+BellonGuillaume@users.noreply.github.com>
2018-05-11 17:47:41 +0200

    Debugage et changement des polices
    11/05/18 - 17:47

--- app/src/main/java/be/lsinf1225/minipoll/activity/NewDilemmeActivity.java
+++ app/src/main/java/be/lsinf1225/minipoll/activity/NewDilemmeActivity.java
@@ -1,15 +1,56 @@
 package be.lsinf1225.minipoll.activity;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import be.lsinf1225.minipoll.R;
 
 public class NewDilemmeActivity extends AppCompatActivity {
 
+    Button button;
+    EditText editText;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_dilemme);
+        button = findViewById(R.id.btn_inviter_ami);
+        setFontBut(button);
+        editText = findViewById(R.id.et_Titre);
+        setFontEdTxt(editText);
+        textView = findViewById(R.id.tv_nouveau_dilemme);
+        setFontTxt(textView);
+    }
+
+    public void setFontTxt(TextView textView) {
+        try {
+            Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Light.ttf");
+            textView.setTypeface(typeface);
+        } catch (Exception e) {
+            Log.e("FONT", textView + " not found", e);
+        }
+    }
+
+    public void setFontBut(Button button) {
+        try {
+            Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Light.ttf");
+            button.setTypeface(typeface);
+        } catch (Exception e) {
+            Log.e("FONT", button + " not found", e);
+        }
+    }
+
+    public void setFontEdTxt(EditText editText) {
+        try {
+            Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Light.ttf");
+            editText.setTypeface(typeface);
+        } catch (Exception e) {
+            Log.e("FONT", editText + " not found", e);
+        }
     }
 }
--- app/src/main/java/be/lsinf1225/minipoll/activity/NewSondageActivity.java
+++ app/src/main/java/be/lsinf1225/minipoll/activity/NewSondageActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -27,15 +28,21 @@ public class NewSondageActivity extends AppCompatActivity {
 
     List<ChoixAmis> liste;
     int count;
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_sondage);
+        textView = findViewById(R.id.tv_nouveau_sondage);
+        setFontTxt(textView);
+        button = findViewById(R.id.btn_inviter_amis);
+        setFontBut(button);
         liste = new ArrayList<>();
         count = 0;
         String mail = MiniPoll.getConnected_user().getMail();
         SQLiteDatabase db = MySQLiteHelper.get().getReadableDatabase();
-        String SQL = "SELECT Utilisateur2 FROM Relation WHERE Utilisateur1 = '"+mail+"', Statut = 'Ami';";
+        String SQL = "SELECT Utilisateur2 FROM Relation WHERE Utilisateur1 = '"+mail+"' and Statut = 'Ami';";
         Cursor c = db.rawQuery(SQL,null);
         c.moveToFirst();
         while(!c.isAfterLast()) {
@@ -50,4 +57,30 @@ public class NewSondageActivity extends AppCompatActivity {
 
     }
 
+    public void setFontTxt(TextView textView) {
+        try {
+            Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Light.ttf");
+            textView.setTypeface(typeface);
+        } catch (Exception e) {
+            Log.e("FONT", textView + " not found", e);
+        }
+    }
+
+    public void setFontBut(Button button) {
+        try {
+            Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Light.ttf");
+            button.setTypeface(typeface);
+        } catch (Exception e) {
+            Log.e("FONT", button + " not found", e);
+        }
+    }
+
+    public void setFontEdTxt(EditText editText) {
+        try {
+            Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Light.ttf");
+            editText.setTypeface(typeface);
+        } catch (Exception e) {
+            Log.e("FONT", editText + " not found", e);
+        }
+    }
 }
